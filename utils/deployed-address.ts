ece5cf187387c06a0c241125218a7160f499e791
selvy elisa <selvyelisa999@gmail.com>
2024-08-17 11:35:33 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x7fCac10FAf73f513CB48fBA8Cfed5b6c06d26848'
+export const ProxyAdmin = '0x33b43D6A76456Df3793a4d34fD2C49E33D733b16'
+export const SWTRImplementation = '0xbA56a719677Afbf713C89068b20845C08cB530F2'
     
